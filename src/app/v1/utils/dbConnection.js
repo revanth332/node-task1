1ff7950e2c5de410a68f4eb6301f61585e9b97c3
revanth332 <revathipathilanka347@gmail.com>
2024-08-14 12:47:11 +0530

    added error handling for db connection

--- src/app/v1/utils/dbConnection.js
+++ src/app/v1/utils/dbConnection.js
@@ -1,25 +1,27 @@
-import config from '../../../../config.js';
-import logger from '../../../../logger.js';
-import mysql from 'mysql2/promise'
+import config from "../../../../config.js";
+import logger from "../../../../logger.js";
+import mysql from "mysql2/promise";
 
 const DbConfig = {
-    host : config.DATABASE_HOST,
-    user : config.DATABASE_USER,
-    password:config.DATABASE_PASSWORD,
-    database : config.DATABASE_NAME
+  host: config.DATABASE_HOST,
+  user: config.DATABASE_USER,
+  password: config.DATABASE_PASSWORD,
+  database: config.DATABASE_NAME,
 };
 
 const poolPromise = (async () => {
-    try {
-        const pool = await mysql.createPool(DbConfig); 
-
-        console.log('Connected to MySQL database successfully!');
-        return pool;
-
-    } catch (err) {
-        logger.info('Database Connection Failed! Bad Config: ', err);
-        // throw err;
+  try {
+    const pool = await mysql.createPool(DbConfig);
+    const sql = "select 1";
+    const [rows] = await pool.query(sql);
+    if (rows.length > 0) {
+      logger.info("Connected to MySQL database successfully!");
+      return pool;
     }
+  } catch (err) {
+    logger.error("Database Connection Failed! Bad Config: ", err);
+    // throw err;
+  }
 })();
 
-export {  poolPromise };
+export { poolPromise };
